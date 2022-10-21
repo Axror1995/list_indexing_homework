761a5c1d148fb794c3fd1df347aa453b80fabd78
aror raximmov <axrorraximov108@gmail.com>
2022-10-21 12:35:37 +0500

    08

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -5,5 +5,23 @@
         list1 (list): parameter
     Returns:
         list: return answer
-*/
-void main() {}
+*/List func(List list1){
+  int s1 = 0;
+  int s2 = 0;
+while(s1 < list1.length){
+if (list1[s1]==1){
+  list1[s1]=true;
+    }
+    else if(s2 < list1.length){
+      if (list1[s2]==1){
+        list1[s2]=false;
+      }
+    }
+    s1 += 2;
+    s2 += 1;
+  }
+  return list1;
+}
+void main() {
+  print(func([1,0,0,0,0]));
+}
